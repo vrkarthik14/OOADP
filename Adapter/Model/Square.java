9781733514eaeb5483f1d5ddce8d188e07bfd823
Vivek V Hathwar <34102731+vivekhathwar@users.noreply.github.com>
2019-04-04 20:49:01 +0530

    Add files via upload

---
+++ Adapter/Model/Square.java
@@ -0,0 +1,23 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Square implements Shape {
+
+    /**
+     * Default constructor
+     */
+    public Square() {
+    }
+
+    /**
+     * 
+     */
+    public void draw() {
+        // TODO implement here
+        System.out.println("A square is drawn");
+    }
+
+}
---
+++ Adapter/Model/Triangle.java
@@ -0,0 +1,23 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Triangle implements Shape {
+
+    /**
+     * Default constructor
+     */
+    public Triangle() {
+    }
+
+    /**
+     * 
+     */
+    public void draw() {
+        // TODO implement here
+        System.out.println("A Triangle is drawn");
+    }
+
+}
